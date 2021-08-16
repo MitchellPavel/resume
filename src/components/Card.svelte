2dc9da244aa80e595516d26ffd2d04465310c2b7
Mitch Pavel <mitchell.pavel@hotmail.com>
2021-08-16 15:33:13 -0500

    feat: add aws cloud practitioner cert

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -5,7 +5,7 @@
     display: inline-block;
     position: relative;
     background-color: var(--light-color);
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 1rem 2rem 0 rgba(0,0,0,0.2);
     transition: 0.3s;
     border-radius: .5em;
     width: 18em;
@@ -13,6 +13,6 @@
     margin: 1em;
   }
   .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 2rem 4rem 0 rgba(0,0,0,0.2);
   }
 </style>
